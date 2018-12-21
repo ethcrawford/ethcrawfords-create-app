c10efbdc49010bded9337bde173215c35629dc83
Ethan Crawford <ethcrawford@gmail.com>
2018-12-21 17:48:07 +0300

    Add html-loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -215,6 +215,13 @@ module.exports = {
         // match the requirements. When no loader matches it will fall
         // back to the "file" loader at the end of the loader list.
         oneOf: [
+          {
+            test: [/\.html$/],
+            loader: require.resolve('html-loader'),
+            options: {
+              attrs: ['img:src', 'link:href', ':data-src'],
+            },
+          },
           // "url" loader works like "file" loader except that it embeds assets
           // smaller than specified limit in bytes as data URLs to avoid requests.
           // A missing `test` is equivalent to a match.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -304,6 +304,13 @@ module.exports = {
         // match the requirements. When no loader matches it will fall
         // back to the "file" loader at the end of the loader list.
         oneOf: [
+          {
+            test: [/\.html$/],
+            loader: require.resolve('html-loader'),
+            options: {
+              attrs: ['img:src', 'link:href', ':data-src'],
+            },
+          },
           // "url" loader works just like "file" loader but it also embeds
           // assets smaller than specified size as data URLs to avoid requests.
           {
