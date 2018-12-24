38f105a66ccae9291aaf7ea2fbc676af60a60218
Ethan Crawford <ethcrawford@gmail.com>
2018-12-24 15:38:57 +0300

    Remove Service Worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import './index.scss';
-import * as serviceWorker from './serviceWorker';
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
